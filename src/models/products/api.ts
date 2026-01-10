eaebe399bc4d66b850424751d80e3460073206c5
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2026-01-10 07:01:47 +0500

    productsApi

---
+++ src/models/products/api.ts
@@ -0,0 +1,13 @@
+export interface IProduct{
+  id: string;
+  productId: string;
+  productName: string;
+  brandName: string;
+  lovesCount: number;
+  rating: number;
+  size: string;
+  ingredients: [],
+  price: number;
+  highlights: [],
+  primaryCategory: string
+}
